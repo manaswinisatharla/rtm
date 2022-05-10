7ec542d823e54bb6529c07564fe0dffa403070de
manaswinisatharla <manaswini.satharla@gmail.com>
2022-05-10 21:09:50 +0530

    jenkins

---
+++ src/test/java/manu_jen/CreateUserTest.java
@@ -0,0 +1,11 @@
+package manu_jen;
+
+import org.testng.annotations.Test;
+
+public class CreateUserTest {
+  @Test
+  public void CreateUserTest() {
+	  
+	 System.out.println("Creating a user"); 
+  }
+}
